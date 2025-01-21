ae6b047d346ac5c66a2560b22ba3d6f717c9c45d
John Rey Decosta <decosta.johnrey30@gmail.com>
2025-01-21 17:30:04 +0800

    update dashboard controller

--- controllers/dashboard.controller.ts
+++ controllers/dashboard.controller.ts
@@ -8,36 +8,115 @@ export const getTotalSummary = async (
   next: NextFunction
 ): Promise<Response> => {
   try {
-    // const data = await prisma.user.aggregate({
-    //   _count: {
-    //     id: true,
-
-    //   },
-
-    //   where: {
-    //     role:'examinee'
-    //   }
-
-    // });
-
-    const data = await prisma.user.count({
-      where: {
-        role: 'examinee'
-      }
-    });
-
-    const exams = await prisma.exam.count({
-      where: {
-        status: true
-      }
-    })
-
-    const course = await prisma.course.count()
-
-    
 
 
-    return res.status(200).json(exams);
+    //for summary
+    const [examineeCount, courseCount, examCount, completedExaminee] = await Promise.all([
+      //for
+      await prisma.user.count({
+        where: {
+          role: 'examinee'
+        }
+      }),
+      await prisma.course.count(),
+      await prisma.exam.count({
+        where: {
+          status: true
+        }
+      }),
+      await prisma.examAttempt.groupBy({
+        by: ['examinee_id'],
+        _count: {
+          exam_id: true
+        },
+        having: {
+          examinee_id: {
+            _count: {
+              equals: await prisma.exam.count({
+                where: { status: true },
+              }),
+            }
+          }
+        }
+      })
+
+    ])
+
+    const totalCompleteExaminee = completedExaminee.map((entry) => entry.examinee_id).length;
+
+
+
+
+    interface RegisterCompletedModel {
+      keyDate: string,
+      count: number,
+    }
+
+
+
+
+
+    const registeredExaminee = await prisma.$queryRaw<RegisterCompletedModel[]>`
+      SELECT 
+      TO_CHAR(DATE("createdAt"), 'Mon DD, YYYY') as keyDate,
+        COUNT(*)::integer as count 
+      FROM 
+        "User" 
+      GROUP BY 
+      keyDate
+      ORDER BY 
+      keyDate ASC;
+    `;
+
+    const completedExaminees = await prisma.$queryRaw<RegisterCompletedModel[]>`
+          SELECT 
+          TO_CHAR(DATE("createdAt"), 'Mon DD, YYYY') as keyDate,
+            COUNT(DISTINCT examinee_id)::integer as cnt
+          FROM 
+            "ExamAttempt"
+
+          GROUP BY
+          keyDate
+
+          ORDER BY 
+          keyDate ASC;
+          `;
+
+          
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    const finalMap = {
+      summary: {
+        registeredExaminee: examineeCount,
+        completedExaminee: totalCompleteExaminee,
+        totalCourse: courseCount,
+        totalExams: examCount
+      },
+      registeredExaminee,
+      completedExaminees
+
+
+
+    }
+
+
+
+
+    return res.status(200).json(finalMap);
   } catch (err: any) {
     return res.status(500).json({
       message: err.message,
